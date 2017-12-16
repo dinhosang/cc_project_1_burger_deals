244639a41a30120703618f741321025b2a5952ca
Douglas Sangster <d.sangster@hotmail.co.uk>
2017-12-16 21:15:40 +0000

    adding further checks for the Deal and Eatory class CRUD methods

--- program/models/specs/checking_sql_methods_with_pry.rb
+++ program/models/specs/checking_sql_methods_with_pry.rb
@@ -1,27 +1,40 @@
 require('pry-byebug')
 require_relative('../burger')
+require_relative('../eatory')
 
 burger1 = Burger.new({'type' => 'beef burger'})
 burger2 = Burger.new({'type' => 'cheese burger', 'name' => 'Big Cheese'})
 burger3 = Burger.new({'type' => 'burger', 'name' => 'Basic B', 'price' => '323'})
 
+eatory1 = Eatory.new({"name" => "Jawsome Burgers"})
+eatory2 = Eatory.new({"name" => "Bricks"})
+eatory3 = Eatory.new({"name" => "Dinner's #here"})
+
+
 burger1.save
 burger2.save
 burger3.save
+eatory1.save
+eatory2.save
+eatory3.save
+
+# binding.pry
 
 burger2.name = "Smaller Cheese"
 burger3.type = "classy burger"
 burger3.name = "Best Burger"
+eatory3.name = "Any other name will do"
 
 burger2.update
 burger3.update
+eatory3.update
+
+eatory1.add_stock([burger1, burger3])
+
+binding.pry
 
 burger1.delete
-
-# binding.pry
-# burger2.delete
-# burger3.delete
-# burgers = Burger.find_all
+eatory2.delete
 
 
 binding.pry
